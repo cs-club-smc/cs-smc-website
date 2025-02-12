4799b063e7de168b3c834970aef7d356d5f1377d
Jake TyCyn <jaketycyn@gmail.com>
2025-02-12 14:57:30 -0800

    fix(vite) - reordered plugins

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite({ autoCodeSplitting: true })],
+  plugins: [TanStackRouterVite({ autoCodeSplitting: true }), react()],
 });
