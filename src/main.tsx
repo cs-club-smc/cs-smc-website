bf909581b17e63537fe73a2bf0a12a33d2e15375
Jake TyCyn <jaketycyn@gmail.com>
2025-02-12 13:03:10 -0800

    fix(global) - tailwindcss w/ vite connected & fixed

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./styles/index.css";
 
-const rootElement = document.getElementById("root");
-if (!rootElement) throw new Error("Failed to find the root element");
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
