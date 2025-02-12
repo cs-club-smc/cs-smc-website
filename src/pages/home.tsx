eafd8c385048fd762249b99e8a485dd568b98bf4
Jake TyCyn <jaketycyn@gmail.com>
2025-02-12 14:57:56 -0800

    feat(tanStack) - Routes & navigation basis changed to TanStack

--- src/pages/home.tsx
+++
@@ -1,9 +0,0 @@
-export const Home = () => {
-  return (
-    <div className="flex flex-col items-center justify-center">
-      <h1>SMC CS CLUB</h1>
-      <h2>Other text big Announcmeent stuff</h2>
-      <h3>Other text smaller announcmenet</h3>
-    </div>
-  );
-};
